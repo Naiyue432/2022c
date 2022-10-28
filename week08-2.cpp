f36052eea40e32e0b3586fa4f8a1febaa03553fa
Naiyue432 <114201496+Naiyue432@users.noreply.github.com>
2022-10-28 12:04:54 +0800

    Add files via upload

---
+++ week08-2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a=1;
+    while(a<=n)
+    {
+        int b=1;
+        while(b<=n)
+        {
+            if( b<=n-a )
+                printf(" ");
+            else
+                printf("*");
+            b++;
+        }
+        printf("\n");
+        a++;
+    }
+}
